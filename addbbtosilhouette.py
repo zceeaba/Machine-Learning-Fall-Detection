e47c42f1dd75c0ff1db58473f06b870c17fc0b5f
Abhishekh Jay baskaran <zceeaba@ucl.ac.uk>
2018-05-05 00:12:35 +0100

    working code to add bounding box/centre to the silhouette images

--- addbbtosilhouette.py
+++ addbbtosilhouette.py
@@ -4,30 +4,44 @@ from matplotlib import pyplot as plt
 
 data=readpickle("testdataforbb.txt")
 def imagewrite():
+    for j in list(data):
+        if len(data[j]) < 2:
+            del data[j]
     count = 0
     for i in data:
-        if count==0:
-            #print(i,data[i])
-            filename='./images/'+'one'+'.png'
-            with open(filename, 'wb') as f:
-                f.write(data[i][0])
+        #if count==0:
+        #print(i,data[i])
+        filename='./images/'+str(count)+'.png'
+        with open(filename, 'wb') as f:
+            f.write(data[i][0])
         count = count+1
 
-count=0
-bb=0
-for j in data:
-    if count==1:
-        bb=data[j][1]
-    count+=1
+    print(data)
 
 import numpy as np
+for j in list(data):
+    if len(data[j]) < 2:
+        del data[j]
+"""
+for i in data:
+    filename = './images/' +str(count)+ '.png'
+    image1 = cv2.imread(filename)
+    bb=data[i][1]
+    cv2.rectangle(image1,(bb[0],bb[1]),(bb[2],bb[3]),(0,255,0),3)
+    cv2.imwrite(filename,image1)
+    count+=1
+"""
+"""
+count=0
+for i in data:
+    filename = './images/' +str(count)+ '.png'
+    image1 = cv2.imread(filename)
+    bb=data[i][2]
+    cv2.circle(image1, (bb[0], bb[1]), 1, (0, 0, 255), -1)
+    cv2.imwrite(filename,image1)
+    count+=1
+"""
 
-filename = './images/' + 'one' + '.png'
-image1 = cv2.imread(filename)
-print(bb)
-cv2.rectangle(image1,(bb[0],bb[1]),(bb[2],bb[3]),(0,255,0),3)
-#image1=image1+image2
-plt.imshow(image1),plt.show()
 
 """"
 filename = './images/' + timestr + "_0_" + str(count) + '.png'  # I assume you have a way of picking unique filenames
