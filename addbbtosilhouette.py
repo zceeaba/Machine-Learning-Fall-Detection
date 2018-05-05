3daa6d0b5d4e5be6d236c2f038b2ac546938bf9c
Abhishekh Jay baskaran <zceeaba@ucl.ac.uk>
2018-05-05 06:37:32 +0100

    ccombined sillhouette detection with groundtruth

--- addbbtosilhouette.py
+++ addbbtosilhouette.py
@@ -1,7 +1,9 @@
 from FallDetect.readpickle import readpickle
 import cv2
 from matplotlib import pyplot as plt
-
+from FallDetect.readsilhouette import readsilhouette
+import pandas as pd
+import math
 #data=readpickle("testdataforbb.txt")
 data=readpickle("finalsillbbdata.txt")
 def imagewrite():
@@ -19,7 +21,7 @@ def imagewrite():
 
     print(data)
 
-
+"""
 import numpy as np
 for j in list(data):
     if len(data[j]) < 2:
@@ -35,7 +37,55 @@ for i in data:
     cv2.rectangle(image1,(bb[0],bb[1]),(bb[2],bb[3]),(0,255,0),3)
     cv2.imwrite(filename,image1)
     count+=1
+"""
+import numpy as np
+for j in list(data):
+    if len(data[j]) < 2:
+        del data[j]
+array = readsilhouette()
+#print(array)
+count=0
+print(len(array))
+length=len(array)
+keys=list(data.keys())
+def determine(value):
+    if value in keys:
+        return False
+    else:
+        return True
+newarray = [x for x in array if not determine(x["time"])]
+bb=[]
+bbcen=[]
+for i in data:
+    bb.append(data[i][1])
+    bbcen.append(data[i][2])
+    count+=1
+def distance(pointa,pointb):
+    dis=math.sqrt(((pointa[0]-pointb[0])**2+(pointa[1]-pointb[1])**2))
+    return dis
 
+def angle(pointa,pointb):
+    y=pointa[1]-pointb[1]
+    x=pointa[0]-pointb[0]
+    angle=math.atan2(y,x)
+    degree=math.degrees(angle)
+    return degree
+
+newarray[0]["bb"]=bb[0]
+newarray[0]["bbcen"]=bb[1]
+newarray[0]["distance"]=0
+newarray[0]["angle"]=0
+
+for j in range(1,len(newarray)):
+    newarray[j]["bb"]=bb[j]
+    newarray[j]["distance"]=distance(bb[j],bb[j-1])
+    newarray[j]["angle"]=angle(bb[j],bb[j-1])
+    newarray[j]["bbcen"]=bbcen[j]
+
+pdarray = pd.DataFrame(newarray)
+
+print(pdarray)
+#gts = pdarray.filter(items=['groundtruthstate', 'imagevalue', 'time'])
 
 """
 count=0
