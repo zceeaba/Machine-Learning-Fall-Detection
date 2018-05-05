2c6f177ab570ea07c09b8260d3e6714c9476c4c0
Abhishekh Jay baskaran <zceeaba@ucl.ac.uk>
2018-05-05 01:02:14 +0100

    working bb+silhouette and filtered data according to our timestamp values

--- addbbtosilhouette.py
+++ addbbtosilhouette.py
@@ -2,7 +2,8 @@ from FallDetect.readpickle import readpickle
 import cv2
 from matplotlib import pyplot as plt
 
-data=readpickle("testdataforbb.txt")
+#data=readpickle("testdataforbb.txt")
+data=readpickle("finalsillbbdata.txt")
 def imagewrite():
     for j in list(data):
         if len(data[j]) < 2:
@@ -11,30 +12,35 @@ def imagewrite():
     for i in data:
         #if count==0:
         #print(i,data[i])
-        filename='./images/'+str(count)+'.png'
+        filename='./imagesb/'+str(count)+'.png'
         with open(filename, 'wb') as f:
             f.write(data[i][0])
         count = count+1
 
     print(data)
 
+
 import numpy as np
 for j in list(data):
     if len(data[j]) < 2:
         del data[j]
-"""
+
+count=0
 for i in data:
-    filename = './images/' +str(count)+ '.png'
+    filename = './imagesb/' +str(count)+ '.png'
     image1 = cv2.imread(filename)
     bb=data[i][1]
+    bbcen=data[i][2]
+    cv2.circle(image1, (bbcen[0], bbcen[1]), 3, (0, 0, 255), -1)
     cv2.rectangle(image1,(bb[0],bb[1]),(bb[2],bb[3]),(0,255,0),3)
     cv2.imwrite(filename,image1)
     count+=1
-"""
+
+
 """
 count=0
 for i in data:
-    filename = './images/' +str(count)+ '.png'
+    filename = './imagesb/' +str(count)+ '.png'
     image1 = cv2.imread(filename)
     bb=data[i][2]
     cv2.circle(image1, (bb[0], bb[1]), 1, (0, 0, 255), -1)
