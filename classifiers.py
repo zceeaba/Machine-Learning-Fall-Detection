3e0500fc61024ae2db6c440d4d80a719d9aebd07
Abhishekh Jay baskaran <zceeaba@ucl.ac.uk>
2018-05-05 01:14:31 +0100

    fixed data and reset wearable classifier for accelerometer data

--- classifiers.py
+++ classifiers.py
@@ -1,5 +1,6 @@
 from FallDetect.dataparser import wearable
 from sklearn import model_selection
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVC
